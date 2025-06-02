b39f3d3b234586152b40b5baffcb0a65c216c7ef
Junior flores <fmjg21@gmail.com>
2025-06-02 19:03:07 +0200

    drop_duplicates, keep first

--- python_script/server.py
+++ python_script/server.py
@@ -52,7 +52,7 @@ def read_csv(csv_path):
     if "date" not in df.columns:
         has_error = True
         print(csv_path, "does not contain date column")
-    df = df.drop_duplicates(subset=["date"])
+    df = df.drop_duplicates(subset=["date"], keep="first")
     df["date_dt"] = df["date"].apply(str2date)
     # df.drop_duplicates(subset=["doy"], inplace=True)
     df.reset_index(drop=True, inplace=True)
